b899ae4e4ed812f0ceeaa8f23cdd606057ddbe2a
KelvynLenis <kmartinslenis30@hotmail.com>
2024-09-24 15:13:25 -0300

    feat: add editor, auth and collaborator

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,52 @@
+'use server'
+
+import { nanoid } from 'nanoid'
+import { liveblocks } from '../liveblocks';
+import { revalidatePath } from 'next/cache';
+import { parseStringify } from '../utils';
+
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
+  const roomId = nanoid()
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: 'untitled'
+    }
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ['room:write']
+    }
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ['room:write']
+    });
+
+    revalidatePath('/')
+
+    return parseStringify(room);
+
+  } catch (error) {
+    console.error(`Error creating document: ${error}`)
+  }
+}
+
+export const getDocument = async ({ roomId, userId }: { roomId: string, userId: string }) => {
+
+  try {
+    const room = await liveblocks.getRoom(roomId)
+
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error('You do not have access to this document')
+    // }
+
+    return parseStringify(room)
+  } catch (error) {
+    console.error(`Error getting document: ${error}`)
+  }
+}
